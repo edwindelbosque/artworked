6ab54b8b4147abd630c622932959967740474244
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-01 20:05:48 -0600

    Style the loader image into the canvas

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Artwork.scss';
+import loadingGif from '../../assets/loadingGif.gif';
 
-const Artwork = ({
-	name,
-	artist,
-	releaseYear,
-	artwork,
-	hqArtwork,
-	isLoaded
-}) => {
-	const loadHandler = () => {
-		isLoaded = true;
+class Artwork extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isLoaded: false
+		};
+	}
+
+	loadHandler = () => {
+		this.setState({
+			isLoaded: true
+		});
 	};
 
-	return (
-		<article className='Artwork'>
-			<a href={hqArtwork} download='test'>
-				<img alt={`${name} album artwork`} src={artwork}>
-					{' '}
-					onLoad={loadHandler}
-				</img>
-			</a>
-			<div>
-				<h3>{artist}</h3>
-				<h3>{name}</h3>
-				<h3>{releaseYear}</h3>
-			</div>
-		</article>
-	);
-};
+	render() {
+		const { isLoaded } = this.state;
+		const { name, artist, releaseYear, artwork, hqArtwork } = this.props;
+		const show = isLoaded ? { display: 'block' } : { display: 'none' };
+		return (
+			<article className='Artwork'>
+				<a href={hqArtwork} download='test'>
+					{!this.state.isLoaded && <img alt='loading gif' src={loadingGif} />}
+					<img
+						alt={`${name} album artwork`}
+						src={artwork}
+						style={show}
+						onLoad={this.loadHandler}
+					/>
+				</a>
+				<div>
+					<h3>{artist}</h3>
+					<h3>{name}</h3>
+					<h3>{releaseYear}</h3>
+				</div>
+			</article>
+		);
+	}
+}
 
 export default Artwork;
