951fb0f90a984c16d0048c2b9358288bb49329e6
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-03 01:27:49 -0600

    Updates

---
+++ precache-manifest.7fb0ca4db4debf0a656aacc3a9d7dc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37f8fe57efd9110054dd1eefa627d34",
+    "url": "/artworked/index.html"
+  },
+  {
+    "revision": "e8a73f44e3bbc6a18378",
+    "url": "/artworked/static/css/main.d631472f.chunk.css"
+  },
+  {
+    "revision": "3667b51d5c9d63fa18b8",
+    "url": "/artworked/static/js/2.ec2a8bd7.chunk.js"
+  },
+  {
+    "revision": "e8a73f44e3bbc6a18378",
+    "url": "/artworked/static/js/main.3d4dba75.chunk.js"
+  },
+  {
+    "revision": "86ad613f213c93952b1c",
+    "url": "/artworked/static/js/runtime-main.d9ecc51c.js"
+  },
+  {
+    "revision": "95cf96ca384892f3ccb0d2a2bbce41f4",
+    "url": "/artworked/static/media/loadingGif.95cf96ca.gif"
+  }
+]);
