e264beb2f2f82434e79c171c51dff876c767406c
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-03 01:33:28 -0600

    Change .json to deploy to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
