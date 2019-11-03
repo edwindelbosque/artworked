2c0b8d3329b922ec1c59d52c9a4a3ada3ccdf642
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-02 22:44:07 -0600

    Make container dynamic by either showing search cards or favorites

---
+++ src/components/Container/Container.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Container.scss';
+import ArtworkResult from '../ArtworkResult/ArtworkResult';
+import loadingGif from '../../assets/loadingGif.gif';
+import { connect } from 'react-redux';
+
+const Container = ({ results, isLoading, isFavorites, favorites }) => {
+	const mapCards = type => {
+		return type.map((result, index) => {
+			const { name, artist, releaseYear, hqArtwork, id } = result;
+			return (
+				<ArtworkResult
+					key={index}
+					id={id}
+					name={name}
+					artist={artist}
+					releaseYear={releaseYear}
+					hqArtwork={hqArtwork}
+					isLoaded={false}
+				/>
+			);
+		});
+	};
+
+	const cardsHandler = isFavorites ? mapCards(favorites) : mapCards(results);
+
+	const loader = (
+		<img
+			rel='preload'
+			alt='loading gif'
+			as='image'
+			src={loadingGif}
+			style={{ width: '600px' }}
+		/>
+	);
+
+	return (
+		<>
+			{isLoading && loader}
+			<article className='Container'>{cardsHandler}</article>
+		</>
+	);
+};
+
+const mapStateToProps = ({ results, isLoading, isFavorites, favorites }) => ({
+	results,
+	isLoading,
+	isFavorites,
+	favorites
+});
+
+export default connect(mapStateToProps)(Container);
